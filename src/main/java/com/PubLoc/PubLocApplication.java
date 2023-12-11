e99ef129460447c083ef1d53e78236846bd716cf
송현우 <shw7349@naver.com>
2023-12-11 20:50:29 +0900

    초기설정

---
+++ src/main/java/com/PubLoc/PubLocApplication.java
@@ -0,0 +1,13 @@
+package com.PubLoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubLocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubLocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PubLoc/PubLocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PubLoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubLocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
